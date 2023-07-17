cd8ed837fc6076b1304edc1f03f22ffbcd728dab
Treasure <133379929+yvestresor@users.noreply.github.com>
2023-07-17 09:50:25 +0300

    Fix the no score to be zero when game resets

--- script.js
+++ script.js
@@ -12,103 +12,22 @@ const main = document.querySelector("main");
 const endAlrt = document.querySelector("#end-alert");
 const endDesc = document.querySelector("#end-desc");
 const returnMainBtn = document.querySelector("#retry-btn");
-const desc = document.querySelector("#desc3");
 const container = document.querySelector("#results-container");
 
-body.addEventListener("click", skipAnime());
-body.addEventListener("keydown", skipAnime());
-
-function skipAnime() {
-  const span = document.querySelectorAll("span");
-
-  span.forEach((span) => span.classList.add("skip"));
-}
 
 function beginningAnimation() {
-  fadeIn();
-  //need to turn nodelist of spans into an array so we can access last value for ontransitionend
-  const desc1 = document.querySelector("#desc1");
-  let desc1Span = desc1.querySelectorAll("span");
+  const cta = document.querySelector("#cta");
+  cta.classList.add("drop-down");
 
-  desc1Span = Array.from(desc1Span);
+  cta.addEventListener("animationend", () => {
+    const gameCtn = document.querySelector("#game-container");
 
-  const desc2 = document.querySelector("#desc2");
-  const desc3 = document.querySelector("#desc3");
-
-  desc1Span[desc1Span.length - 1].ontransitionend = () => {
-    desc1.classList.add("fade-out");
-
-    desc1.addEventListener("animationend", () => {
-      desc1.classList.add("disappear");
-      desc1.classList.remove("animate");
-      desc2.classList.remove("disappear");
-      desc2.classList.add("animate");
-      fadeIn();
-      /* need to collect nodelist of span 
-in the same function we activate fadein()
-or else nodelist will be empty */
-      let desc2Span = desc2.querySelectorAll("span");
-      desc2Span = Array.from(desc2Span);
-
-      desc2Span[desc2Span.length - 1].ontransitionend = () => {
-        desc2.classList.add("fade-out");
-        desc2.addEventListener("animationend", () => {
-          desc2.classList.add("disappear");
-          desc2.classList.remove("animate");
-          desc3.classList.remove("disappear");
-          desc3.classList.add("animate");
-          fadeIn();
-
-          let desc3Span = desc3.querySelectorAll("span");
-          desc3Span = Array.from(desc3Span);
-
-          desc3Span[desc3Span.length - 1].ontransitionend = () => {
-            const cta = document.querySelector("#cta");
-
-            cta.classList.add("drop-down");
-
-            cta.addEventListener("animationend", () => {
-              const gameCtn = document.querySelector("#game-container");
-
-              setTimeout(function () {
-                gameCtn.classList.add("fade-in");
-              }, 300);
-            });
-          };
-        });
-      };
-    });
-  };
+    setTimeout(function () {
+      gameCtn.classList.add("fade-in");
+    }, 300);
+  });
 }
-function fadeIn() {
-  let text = document.querySelector(".animate");
 
-  let strText = text.textContent;
-  let splitText = strText.split("");
-  text.textContent = "";
-  //append span tags to each character in the string
-  for (i = 0; i < splitText.length; i++) {
-    text.innerHTML += `<span>${splitText[i]}</span>`;
-  }
-
-  let char = 0;
-  let timer = setInterval(onTick, 50);
-
-  function onTick() {
-    const span = text.querySelectorAll("span")[char];
-    span.classList.add("fade");
-    char++;
-    //stops the function from running once the end of the string has been reached
-    if (char === splitText.length) {
-      complete();
-      return;
-    }
-  }
-  function complete() {
-    clearInterval(timer);
-    timer = null;
-  }
-}
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -229,44 +148,29 @@ function declareWinner() {
     endDesc.textContent = "You lost...who will save mankind now?";
     returnMainBtn.innerText = "Try Again?";
   }
-  fadeIn();
-
-  let endDescSpan = endDesc.querySelectorAll("span");
-  endDescSpan = Array.from(endDescSpan);
-
-  endDescSpan[endDescSpan.length - 1].ontransitionend = () => {
-    returnMainBtn.classList.add("fade-in");
-    /*returnMainBtn.animate([{ opacity: 0 }, { opacity: 1 }], {
-      duration: 00,
-      fill: "forwards",
-      iterations: 1,
-      delay: 0,
-      easing: "ease-in",
-    });*/
-  };
+  returnMainBtn.classList.add("fade-in");
 }
 
 function rplContent() {
   main.classList.add("disappear");
   endAlrt.classList.remove("disappear");
-  desc.classList.remove("animate");
   endDesc.classList.add("animate");
 
   returnMainBtn.addEventListener("click", () => {
     main.classList.remove("disappear");
     endAlrt.classList.add("disappear");
-    desc.classList.add("animate");
     returnMainBtn.classList.remove("fade-in");
     resetGame();
   });
 }
 
 function resetGame() {
-  fadeIn();
   container.textContent = "";
   playerScore = 0;
+  noScore = 0;
   computerScore = 0;
   keepPlayerScore();
+  keepNoScore();
   keepCpuScore();
 }
 
